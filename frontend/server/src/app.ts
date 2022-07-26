776ce349794a1c3bdf40b9d3fdeede604c63b828
JupiterPi <jupiterpi@outlook.de>
2022-07-26 16:53:38 +0200

    serving webapp

--- frontend/server/src/app.ts
+++ frontend/server/src/app.ts
@@ -1,1 +1,23 @@
-console.log("works");
+const express = require("express");
+const app = express();
+const port = 63108;
+
+app.use(express.json());
+
+// --------
+const _app_folder = "dist/vocabulum-webapp-pwa";
+// ---- SERVE STATIC FILES ---- //
+//app.get('*.*', express.static(_app_folder));
+// ->
+app.use(express.static("dist/vocabulum-webapp-pwa"));
+// ---- SERVE APPLICATION PATHS ---- //
+[].forEach(route => {
+    app.get(route, function (req, res) {
+        res.status(200).sendFile("/index.html", {root: _app_folder});
+    });
+});
+// --------  (source: https://itnext.io/express-server-for-an-angular-application-part-1-getting-started-2cd27de691bd)
+
+app.listen(port, () => {
+    console.log(`Vocabulum Webapp PWA server listening on port ${port}`);
+});
