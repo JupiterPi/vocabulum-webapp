30ddaaed1be42222b40bc756f4ba1c4c9c05c866
JupiterPi <jupiterpi@outlook.de>
2022-10-04 21:28:48 +0200

    simple history on profile page

--- frontend/app/src/app/data/user.service.ts
+++ frontend/app/src/app/data/user.service.ts
@@ -6,6 +6,13 @@ export type UserDetails = {
   email: string
 };
 
+export type HistoryItem = {
+  time: Date,
+  mode: "cards" | "chat",
+  direction: "lg" | "rand" | "gl",
+  selection: string
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,7 +24,38 @@ export class UserService {
     email: "jupiterpi@vocabulum.de"
   };
 
+  devHistory: HistoryItem[] = [
+    {
+      time: new Date(0),
+      mode: "cards",
+      direction: "lg",
+      selection: "37"
+    },
+    {
+      time: new Date(0),
+      mode: "cards",
+      direction: "gl",
+      selection: "38"
+    },
+    {
+      time: new Date(),
+      mode: "chat",
+      direction: "rand",
+      selection: "39"
+    },
+    {
+      time: new Date(),
+      mode: "chat",
+      direction: "gl",
+      selection: "40"
+    }
+  ];
+
   getUserDetails() {
     return this.devUserDetails;
+  }
+
+  getHistory() {
+    return this.devHistory;
   }
 }
--- frontend/app/src/app/ui/account/profile/profile.component.ts
+++ frontend/app/src/app/ui/account/profile/profile.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import {UserService, UserDetails} from "../../../data/user.service";
+import {UserService, UserDetails, HistoryItem} from "../../../data/user.service";
+
+type HistoryBlock = {
+  startTime: Date,
+  items: HistoryItem[]
+};
 
 @Component({
   selector: 'app-profile',
@@ -9,12 +14,15 @@ import {UserService, UserDetails} from "../../../data/user.service";
 export class ProfileComponent {
   constructor(private user: UserService) {
     this.userDetails = user.getUserDetails();
+    this.history = user.getHistory();
+    this.sortHistoryItems(this.history);
   }
 
   userDetails: UserDetails = {
     username: "...",
     email: "..."
   };
+  history: HistoryItem[] = [];
 
   obfuscateEmail(email: string) {
     const atIndex = email.lastIndexOf("@");
@@ -34,5 +42,61 @@ export class ProfileComponent {
     prompt("Gib dein aktuelles Passwort ein:");
     prompt("Gib jetzt ein neues Passwort ein:");
     prompt("Wiederhole das neue Passwort:");
+  }
+
+  sortedHistory: HistoryBlock[] = [];
+  sortHistoryItems(history: HistoryItem[]) {
+    this.sortedHistory = [];
+    let previousTime: Date = history[0].time;
+    let block: HistoryBlock = {
+      startTime: history[0].time,
+      items: []
+    };
+    for (let item of history) {
+      if (item.time.getTime() - previousTime.getTime() > 30 * 60 * 1000) {
+        this.sortedHistory.push(block);
+        block = {
+          startTime: item.time,
+          items: [item]
+        };
+      } else {
+        block.items.push(item);
+      }
+      previousTime = item.time;
+    }
+    this.sortedHistory.push(block);
+  }
+
+  formatTime(time: Date) {
+    if (time.getTime() == new Date(0).getTime()) {
+      return "Heute 15:36";
+    } else {
+      return "Heute 17:21";
+    }
+  }
+
+
+  formatMode(mode: "cards" | "chat") {
+    if (mode == "cards") {
+      return "Karteikarten";
+    } else if (mode == "chat") {
+      return "Chat";
+    }
+    return null;
+  }
+
+  formatDirection(direction: "lg" | "rand" | "gl") {
+    if (direction == "lg") {
+      return "Lat. → DE";
+    } else if (direction == "rand") {
+      return "zufälliger Richtung";
+    } else if (direction == "gl") {
+      return "DE → Lat.";
+    }
+    return null;
+  }
+
+  recallHistoryItem(item: HistoryItem) {
+    alert("repeating");
   }
 }
