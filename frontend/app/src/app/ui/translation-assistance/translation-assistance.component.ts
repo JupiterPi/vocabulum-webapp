4136f2d6a4b5a28f61fd977a3c24d8f5d84a32ed
JupiterPi <jupiterpi@outlook.de>
2022-10-14 18:21:09 +0200

    vkpro banner on translation assistance

--- frontend/app/src/app/ui/translation-assistance/translation-assistance.component.ts
+++ frontend/app/src/app/ui/translation-assistance/translation-assistance.component.ts
@@ -11,7 +11,66 @@ export class TranslationAssistanceComponent {
   query: string = "";
   ready: boolean = false;
   error: boolean = false;
-  items: TAItem[] = [];
+  items: TAItem[] = [
+    {
+      title: "Asini",
+      punctuation: false,
+      inflexible: false,
+      forms: ["Gen. Sg.", "Nom. Pl."],
+      definition: "asinus, asini m.",
+      translations: ["der Esel", "das Maultier"],
+      base_form: "asinus"
+    },
+    {
+      title: "stant",
+      punctuation: false,
+      inflexible: false,
+      forms: ["3. Pers. Pl. Präs."],
+      definition: "stare",
+      translations: ["stehen", "dastehen"],
+      base_form: "stare"
+    },
+    {
+      title: ",",
+      punctuation: true,
+      inflexible: false,
+      forms: [],
+      definition: "",
+      translations: [],
+      base_form: ""
+    },
+    {
+      title: "et",
+      punctuation: false,
+      inflexible: true,
+      forms: [],
+      definition: "et",
+      translations: ["und"],
+      base_form: "et"
+    },
+    {
+      title: "exspectant",
+      punctuation: false,
+      inflexible: false,
+      forms: ["3. Pers. Pl. Präs."],
+      definition: "exspectare",
+      translations: ["erwarten", "warten auf"],
+      base_form: "exspectare"
+    },
+    {
+      title: ".",
+      punctuation: true,
+      inflexible: false,
+      forms: [],
+      definition: "",
+      translations: [],
+      base_form: ""
+    }
+  ];
+
+  hasPro = false;
+  usesLeft = 3;
+  unlocked = false;
 
   constructor(private router: Router, private data: DataService) {}
 
